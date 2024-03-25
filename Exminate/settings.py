e03d742d9a1bdcb4592eda71807275eb0e02f1ad
samad-sust <masamad.sust@gmail.com>
2024-03-25 15:12:47 +0600

    configure for static files

--- Exminate/settings.py
+++ Exminate/settings.py
@@ -120,7 +120,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_ALLOW_ALL = True
