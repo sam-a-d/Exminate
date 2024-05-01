a9022dd5781ec9fcdb4002a9bc3c0c4415309070
samad-sust <masamad.sust@gmail.com>
2024-05-01 17:11:49 +0600

    crtd teacher model and edted field model

--- root/models.py
+++ root/models.py
@@ -5,9 +5,20 @@ from django.db import models
 class Field(models.Model):
     name = models.CharField(max_length = 100)
 
+    def __str__(self):
+        return self.name
+
 class Topic(models.Model):
     field = models.ForeignKey(Field, on_delete = models.CASCADE)
     name = models.CharField(max_length = 100)
 
     def __str__(self):
         return self.name
+    
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    # Affiliation = 
+    # Designation = 
+    contact = models.CharField(max_length=20)
+    email = models.EmailField()
+    whatsapp = models.CharField(max_length=20)
