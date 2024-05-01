eedcba05be5c9f7262d33a5e3615389f29cd8cb8
samad-sust <masamad.sust@gmail.com>
2024-05-01 16:17:57 +0600

    move 2 models to root app

--- exam/models.py
+++ exam/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 
+# Imports
+from root.models import Topic, Field
+
 # Create your models here.
 
-class Field(models.Model):
-    name = models.CharField(max_length = 100)
-
-class Topic(models.Model):
-    field = models.ForeignKey(Field, on_delete = models.CASCADE)
-    name = models.CharField(max_length = 100)
-
-    def __str__(self):
-        return self.name
 
 DIFFICULTY_CHOI = (
     ('easy', 'easy'),
--- root/models.py
+++ root/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Field(models.Model):
+    name = models.CharField(max_length = 100)
+
+class Topic(models.Model):
+    field = models.ForeignKey(Field, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
