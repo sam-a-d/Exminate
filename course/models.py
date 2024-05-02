cf944f73b856861f30b00d7ec7e965e3bd09fd0d
samad-sust <masamad.sust@gmail.com>
2024-05-02 17:51:19 +0600

    modified course model

--- course/models.py
+++ course/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
 # imports
-from exam.models import Exam
+from root.models import Teacher, Field
 
 # Create your models here.
 class Course(models.Model):
+    
     title = models.CharField(max_length=200)
     code = models.CharField(max_length=20)
-    # teacher = 
-    # area = 
-    exams = models.ManyToManyField(Exam)
+    teacher = models.ManyToManyField(Teacher)
+    field = models.ManyToManyField(Field)
     description = models.TextField()
 
+    total_marks = models.PositiveIntegerField(default=100)
 
     def __str__(self) -> str:
         return self.title
