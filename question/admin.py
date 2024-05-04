7c8562398c68ef47636336c9a3f3f31b68ff648a
samad-sust <masamad.sust@gmail.com>
2024-05-04 15:49:43 +0600

    register long and short qs to admin

--- question/admin.py
+++ question/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Tag, MCQ
+from .models import Tag, MCQ, ShortQues, LongQues
 # Register your models here.
 
-admin.site.register([Tag, MCQ])
+admin.site.register([Tag, MCQ, ShortQues, LongQues])
