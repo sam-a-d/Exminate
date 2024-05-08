9efbb80e1483ad66de3f77c7cd9f6eb8b6b6aee1
samad-sust <masamad.sust@gmail.com>
2024-05-08 16:57:04 +0600

    created exams list view

--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+# custom imports 
+from .models import Exam
 
 # Create your views here.
+class CourseListView(ListView):
+    model = Exam
+    context_object_name = 'exams'
+    template_name = "exam/exams.html"
