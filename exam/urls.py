2ba4dc092ee719bb9719aa015b6ef23afc0c8569
samad-sust <masamad.sust@gmail.com>
2024-05-08 16:56:20 +0600

    update proj url file and ad a new url in exam app

--- exam/urls.py
+++ exam/urls.py
@@ -15,13 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('root.urls')),
-    path('course/', include('course.urls')),
-    path('exam/', include('exam.urls')),
-    path('admin/', admin.site.urls),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    path('exams/', views.CourseListView.as_view(), name="exams"),
+    
+    
+]
