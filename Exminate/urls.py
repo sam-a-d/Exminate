2ce3a126782f1572cb7dcdb5fda37f9de97dfc47
samad-sust <masamad.sust@gmail.com>
2024-05-08 16:04:46 +0600

    include course url in examinate project

--- Exminate/urls.py
+++ Exminate/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('root.urls')),
+    path('course/', include('course.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
