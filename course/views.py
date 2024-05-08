e8c47be638f7f28f5b14677c98ce3cab7037a6c6
samad-sust <masamad.sust@gmail.com>
2024-05-08 16:05:37 +0600

    created course list view

--- course/views.py
+++ course/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+# Custom Imports
+from .models import Course
 
 # Create your views here.
+class CourseListView(ListView):
+    model = Course
+    template_name = "course/couse-catalog.html"
