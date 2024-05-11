fe709db36eee2984cdd801c31c98696c484a9bcf
samad-sust <masamad.sust@gmail.com>
2024-05-11 15:40:53 +0600

    register course verification token to admin site

--- course/admin.py
+++ course/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Course
+from .models import Course, CourseVerificationToken
 # Register your models here.
-admin.site.register([Course,])
+admin.site.register([Course,CourseVerificationToken])
