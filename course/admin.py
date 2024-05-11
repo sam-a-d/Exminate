d21a8e06b244213b8b2f8a13e2196716f66a93b5
samad-sust <masamad.sust@gmail.com>
2024-05-11 17:45:17 +0600

    register courseHistory model to admin

--- course/admin.py
+++ course/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Course, CourseVerificationToken
+from .models import Course, CourseVerificationToken, CourseHistory
 # Register your models here.
-admin.site.register([Course,CourseVerificationToken])
+admin.site.register([Course,CourseVerificationToken, CourseHistory])
