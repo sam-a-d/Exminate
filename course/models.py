8d7a5e5f4a7c03d152052c37a96c7a2706d6b44f
samad-sust <masamad.sust@gmail.com>
2024-05-11 15:35:12 +0600

    course verification token model created

--- course/models.py
+++ course/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # imports
 from root.models import Teacher, Field
@@ -17,3 +18,19 @@ class Course(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+# Course verification token
+class CourseVerificationToken(models.Model):
+
+    token_code = models.CharField(max_length=10, null=False, default='BS123')
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE),
+    valid_from = models.DateTimeField(default=timezone.now)
+    valid_to = models.DateTimeField(null=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "coursevertoken"
+        verbose_name_plural = "coursevertokens"
+
+    def __str__(self):
+        return self.token_code
