5ce0d6dad0cff4a09d1143b4853c15ea9735c737
samad-sust <masamad.sust@gmail.com>
2024-05-11 17:47:15 +0600

    aded 'taken-courses' context and send to the template (courselist view updated)

--- course/views.py
+++ course/views.py
@@ -1,9 +1,10 @@
+from typing import Any
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 # Custom Imports
-from .models import Course
+from .models import Course, CourseHistory
 from exam.models import Exam
 
 # Create your views here.
@@ -11,6 +12,16 @@ class CourseListView(ListView):
     model = Course
     context_object_name = 'courses'
     template_name = "course/course-catalog.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context =  super().get_context_data(**kwargs)
+        user = self.request.user
+        user_course_history = CourseHistory.objects.filter(user=user)
+        taken_courses = set(history.course_id for history in user_course_history)
+        context['taken_courses'] = taken_courses
+        
+        print(taken_courses)
+        return context
 
 class CourseDetailView(DetailView):
     model = Course
