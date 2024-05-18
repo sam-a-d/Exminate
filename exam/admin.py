2b4a80ad44bc5de9f6b8534194d6ae9a86453eb6
Samad-sust <masamad.sust@gmail.com>
2024-05-18 12:29:55 +0600

    registered examhistorymodel to app

--- exam/admin.py
+++ exam/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from exam.models import Exam, Topic, Field, ExamHistory
+from exam.models import Exam, Topic, Field, ExamHistory, LongQExamHistory
 
 # Register your models here.
-admin.site.register([Exam, ExamHistory])
+admin.site.register([Exam, ExamHistory, LongQExamHistory])
