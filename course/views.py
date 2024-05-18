7af09be87443fffe293d1eccef49f461af4b8a26
Samad-sust <masamad.sust@gmail.com>
2024-05-18 08:55:41 +0600

    beta: implemented course enrolment process in view

--- course/views.py
+++ course/views.py
@@ -2,10 +2,12 @@ from typing import Any
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.utils import timezone
 
 # Custom Imports
-from .models import Course, CourseHistory
+from .models import Course, CourseHistory, CourseVerificationToken
 from exam.models import Exam
+from .forms import CourseEnrolmentForm
 
 # Create your views here.
 class CourseListView(ListView):
@@ -32,3 +34,49 @@ class CourseDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['exams'] = Exam.objects.filter(course=self.kwargs.get('pk'))
         return context
+
+def courseEnrolmentView(request, courseID=None):
+    
+    if request.POST:
+        form = CourseEnrolmentForm(request.POST)
+        if form.is_valid():
+
+            user = request.user
+            course_code = form.cleaned_data['course_code']
+            verification_token = form.cleaned_data['verification_token']
+
+            if CourseVerificationToken.objects.filter(course__code=course_code, token_code = verification_token).exists():
+                token = CourseVerificationToken.objects.get(course__code=course_code, token_code = verification_token)
+                course = Course.objects.get(code=course_code)
+                if  token.no_of_tokens >= 1 and \
+                    token.valid_from <= timezone.now() and \
+                    token.valid_to >= timezone.now() and \
+                    token.is_active == True:
+
+                    CourseHistory.objects.create(
+                        course=course,
+                        user = user,
+                        veri_code_used=verification_token
+                    )
+                else:
+                    print("Token is not valid")
+
+            else:
+                print('Code/ Token Do not Exist')
+            
+        else:
+            pass
+    
+    else:
+        initial_data = {'course_code' : courseID} if courseID else {}
+        form = CourseEnrolmentForm(initial=initial_data)
+    
+    context = {
+        'form' : form,
+        'course_id': courseID
+    }
+    return render(request, template_name='course/course-enrolment.html', context=context)
+
+def courseEnrolmentOutcome(request):
+
+    return render(request, template_name='course/course-enrolment-outcome.html')
