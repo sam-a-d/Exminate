1113607391c9bdd8d4eaf930f655324856cf9fe8
Samad-sust <masamad.sust@gmail.com>
2024-07-08 21:17:32 +0600

    aded McqAndShortQExamHistory into the admin dashboard

--- exam/admin.py
+++ exam/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from exam.models import Exam, Topic, Field, ExamHistory, LongQExamHistory
+from exam.models import Exam, Topic, Field, ExamHistory, LongQExamHistory, McqAndShortQExamHistory
 
 # Register your models here.
-admin.site.register([Exam, ExamHistory, LongQExamHistory])
+admin.site.register([Exam, ExamHistory, LongQExamHistory, McqAndShortQExamHistory ])
